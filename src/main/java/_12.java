a5f66c179e75877e9fca73dca2ea5ed342b18d11
Mateen <makjavaprogrammer@gmail.com>
2022-11-17 14:38:29 +0530

    added mono demo classes

---
+++ src/main/java/_12.java
@@ -0,0 +1,79 @@
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+public class _12 {
+    private static long delay = 4000l;
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        System.out.println(asdf());
+        long end= System.currentTimeMillis();
+        System.out.println(end-start);
+
+    }
+
+    public static String asdf(){
+        return Mono.zip(
+                        Mono.fromCallable(() -> _12.getString1())
+                                .subscribeOn(Schedulers.parallel()),
+                        Mono.fromCallable(() -> _12.getString2())
+                                .subscribeOn(Schedulers.parallel()),
+                        Mono.fromCallable(() -> _12.getString3())
+                        .onErrorResume(e->Mono.error( new RuntimeException(e.getCause()+"_5.getString3()")))
+                        .subscribeOn(Schedulers.parallel())
+                        .map(i->i.toUpperCase()),
+                        Mono.fromCallable(() -> _12.getString4())
+                                .subscribeOn(Schedulers.parallel()),
+                        Mono.fromCallable(() -> _12.getString1())
+                                .subscribeOn(Schedulers.parallel()),
+                        Mono.fromCallable(() -> _12.getString2())
+                                .subscribeOn(Schedulers.parallel()),
+                        Mono.fromCallable(() -> _12.getString3())
+                                .onErrorResume(e->Mono.error( new RuntimeException(e.getCause()+"_5.getString3()")))
+                                .subscribeOn(Schedulers.parallel())
+                                .map(i->i.toUpperCase()),
+                        Mono.fromCallable(() -> _12.getString4())
+                                .subscribeOn(Schedulers.parallel())
+                )
+
+                .map(res->res.getT1()+" "+ res.getT4()+" "+ res.getT3()+" "+
+                        res.getT4()+" "+ res.getT5()+" "+ res.getT6()+" "+
+                        res.getT7()+" "+ res.getT8())
+                .onErrorResume(e->Mono.error(new RuntimeException("test2")))
+                .block();
+
+    }
+
+    private static String getString1(){
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "1";
+    }
+    private static String getString2(){
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }return "2";
+    }
+    private static String getString3(){
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "a3";
+//        throw new RuntimeException("asdfasdfasdf");
+    }
+    private static String getString4(){
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }return "4";
+    }
+
+
+}
